59acd6495c45842c65c440a42c6247d8a3291b17
League Student <league@iMac-8.attlocal.net>
2018-10-07 11:25:37 -0700

    DOGGO BUT NO CUBES
    ALMOST THERE

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -37,18 +37,31 @@ JFrame frame = new JFrame();
 		// 4. find 2 images and save them to your project’s default package
 		
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+String dog = "a dogs finger print.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+JLabel label;
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+label = loadImageFromComputer(dog);
 		// 8. add your JLabel to the frame
+frame.add(label);
 		// 9. call the pack() method on the frame
+frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
-		// 12. remove everything from the frame that was added earlier
+			System.out.println("Clicked!");
+				// 12. remove everything from the frame that was added earlier
+			frame.removeAll();
 		// 13. load a new image like before (this is more than one line of code)
+	String fived = "3d or 2d.jpg";
+	JLabel labeltwo;
+	labeltwo = loadImageFromComputer(fived);
+	frame.add(labeltwo);
 		// 14. pack the frame
+	frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
