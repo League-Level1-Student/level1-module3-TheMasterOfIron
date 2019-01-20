c0d5fd16f0b2c6b03ff220daf248f59e1a34ec11
67tasman <league@worthymini-5.attlocal.net>
2019-01-20 11:29:47 -0800

    MOVEis

--- src/NETFLIXMOVIE.java
+++ src/NETFLIXMOVIE.java
@@ -1,6 +1,21 @@
 
 public class NETFLIXMOVIE {
 public static void main(String[] args) {
-	Movie("Bee Movie",100);
+	Movie m1 = new Movie("Fortnite The Movie",-100);
+	Movie m2 = new Movie("2018 Youtube Rewind",-50);
+	Movie m3 = new Movie("The Dead Memes Movie",0);
+	Movie m4 = new Movie("Burger King Foot Lettuce",15);
+	Movie m5 = new Movie("Bee Movie",25);
+	Movie m6 = new Movie("Never Dying Hope",50);
+	Movie m7 = new Movie("The 90s",90);
+	Movie m8 = new Movie("P VS T",81000000); 
+	m1.getTicketPrice();
+	m2.getTicketPrice();
+	m3.getTicketPrice();
+	m4.getTicketPrice();
+	m5.getTicketPrice();
+	m6.getTicketPrice();
+	m7.getTicketPrice();
+	m8.getTicketPrice();
 }
 }
