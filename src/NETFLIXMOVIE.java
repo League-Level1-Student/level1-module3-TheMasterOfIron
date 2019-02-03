a47aa36e970398872bd58886e65ad2986af2cf47
knowdawg <>
2019-02-03 11:29:14 -0800

    NETFLIX IS GUUUUUUUUUD
    Yes it is.

--- src/NETFLIXMOVIE.java
+++ src/NETFLIXMOVIE.java
@@ -1,3 +1,4 @@
+import javax.swing.JOptionPane;
 
 public class NETFLIXMOVIE {
 public static void main(String[] args) {
@@ -8,7 +9,7 @@ public static void main(String[] args) {
 	Movie m5 = new Movie("Bee Movie",25);
 	Movie m6 = new Movie("Never Dying Hope",50);
 	Movie m7 = new Movie("The 90s",90);
-	Movie m8 = new Movie("P VS T",82000000); 
+	Movie m8 = new Movie("P VS T",83000000); 
 	m1.getTicketPrice();
 	NetflixQueue q = new NetflixQueue();
 	q.addMovie(m1);
@@ -20,6 +21,14 @@ public static void main(String[] args) {
 	q.addMovie(m7);
 	q.addMovie(m8);
 	q.printMovies();
-	q.getBestMovie();
+	q.sortMoviesByRating();
+	JOptionPane.showMessageDialog(null, "The best movie is " + q.getBestMovie() + ".");
+	JOptionPane.showMessageDialog(null, "The second best movie is " + q.getMovie(1) + ".");
+	JOptionPane.showMessageDialog(null, "The third best movie is " + q.getMovie(2) + ".");
+JOptionPane.showMessageDialog(null, "The fourth best movie is " + q.getMovie(3) + ".");
+JOptionPane.showMessageDialog(null, "The fourth worst movie is " + q.getMovie(4) + ".");
+JOptionPane.showMessageDialog(null, "The third worst movie is " + q.getMovie(5) + ".");
+JOptionPane.showMessageDialog(null, "The second worst movie is " + q.getMovie(6) + ".");
+JOptionPane.showMessageDialog(null, "The worst movie is " + q.getMovie(7) + ".");
 }
 }
